0c1f648825a881e2bd51cfcd90b2c48c80240032
arvid <pl.arvid@gmail.com>
2026-04-23 21:54:46 +0200

    Initial commit with project setup (Razor Pages, Identity, Database) and Recipe model.

---
+++ Models/Recipe.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MealPlanner.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Ingredients { get; set; }
+        public string Instructions { get; set; }
+    }
+}
