0e3430e62fc3c18ca32c0d7bd7c87ad8b2f7289e
arvid <pl.arvid@gmail.com>
2026-04-24 12:21:01 +0200

    Added Meal randomizer.

---
+++ Pages/RandomMeal/Index.cshtml.cs
@@ -0,0 +1,35 @@
+using MealPlanner.Data;
+using MealPlanner.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace MealPlanner.Pages.RandomMeal
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Meal? RandomMeal { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var meals = await _context.Meals
+                .Where(m => m.UserId == userId)
+                .ToListAsync();
+
+            if (meals.Any())
+            {
+                var random = new System.Random();
+                RandomMeal = meals[random.Next(meals.Count)];
+            }
+        }
+    }
+}
